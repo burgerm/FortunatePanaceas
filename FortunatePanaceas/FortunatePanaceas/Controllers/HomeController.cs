3be728ecabb142cea6c6413333028177628c6372
burgerm <m.burger.work@gmail.com>
2013-09-26 02:01:24 +0200

    My first commit of FortunatePanaceas

---
+++ FortunatePanaceas/FortunatePanaceas/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FortunatePanaceas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // TODO: This message should come from a database
+            ViewBag.Message = "Welcome to Fortunate Panaceas!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
